2ceb6614619e331c30b8a633383134ad26042cc7
Mohd Faijan <55624756+faiz3ace@users.noreply.github.com>
2023-01-20 14:54:14 +0000

    Added File and Code

---
+++ mainWindow/MainWin.java
@@ -0,0 +1,19 @@
+// package src.main.window;
+import javax.swing.JFrame;
+import javax.swing.JMenuItem;
+
+public class MainWin{
+    public MainWin(){
+        JFrame jframe=new JFrame();
+        // JMenuBar jMenuBar = new JMenuBar();
+        // JMenu jMenuMain = new JMenu("Main");
+        // jframe.setLayout(null);
+        // jframe.setJMenuBar(jMenuBar);
+
+        // JMenuItem jMenuItem = new JMenuItem();
+        jframe.setVisible(true);
+    }
+    public static void main(String args[]){
+        MainWin mWin=new MainWin();
+    }
+}
